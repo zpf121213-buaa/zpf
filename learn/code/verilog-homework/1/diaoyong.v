041eff8b4ff9f7cade49cc95d94f2d4c06daf81c
zpf121213-buaa <zpf121213@buaa.edu.cn>
2021-10-13 04:05:54 +0800

    delete

--- learn/code/verilog-homework/1/diaoyong.v
+++
@@ -1,8 +0,0 @@
-
-module top_module(a,b,out);
-input a,b;
-output out;
-
-mod_a u1(.in1(a),.in2(b),.out(out));
-
-endmodule
--- learn/code/verilog-homework/3/1.v
+++
@@ -1,7 +0,0 @@
-
-module top_module(x,y,z);
-input x,y;
-output z;
-
-assign z=~(x&~y|~x&y);
-endmodule
